3f484be168cbf24de352e172a1157940d9b8c191
otumian <popecan1000@gmail.com>
2019-08-27 03:52:31 +0000

    arithmatic.py - initial commit

---
+++ arithmatic.py
@@ -0,0 +1,37 @@
+# Author: Otumian (Empire)
+# Github account: https://github.com/Otumian-empire
+# Email: popecan1000@gmail.com
+# Library name: Arithmatic
+# Arithmatic: an abstraction layer on the math lib
+# 	Description:
+#		for basic arithmatic operations such as:
+# 	addition, subtraction, multiplication, division, floor division,
+# 	modulo, power
+# 	aside from that, it is the math std-lib
+# Resource: 
+#		https://docs.python.org/3.6/library/math.html?highlight=math#module-math
+# 	https://timothybramlett.com/How_to_create_a_Python_Package_with___init__py.html
+
+class Arithmatic:
+	def __init__(self):
+		print("imported Arithmatic: successful")
+		
+	"""
+		function: add
+		argument: iterable
+		use: add([1,2,3,4,5])
+		description: returns the sum of the iterable, this is similar to sum
+	"""
+	def add(self, iterable_arg):
+		try:
+			val_sum = 0
+			for i in iterable_arg:
+				val_sum += i
+				
+			# return val_sum
+		except Exception as e:
+			return e
+		finally:
+			return val_sum
+			
+
