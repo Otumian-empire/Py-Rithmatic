7e7d23d4e2efc58c3023df18a110fab921b030bf
otumian <popecan1000@gmail.com>
2019-08-29 13:39:06 +0000

    formatted all the files

--- arithmatic.py
+++ arithmatic.py
@@ -9,137 +9,143 @@
 # 	addition, subtraction, multiplication, division, floor division,
 # 	modulo, power
 
+
 class Arithmatic:
-	""" Arithmatic: an abstraction layer on the arithmatic operators """
+    """ Arithmatic: an abstraction layer on the arithmatic operators """
 
-	def __init__(self):
-		print("imported Arithmatic: successful")
+    def __init__(self):
+        print("imported Arithmatic: successful")
 
-	def add(self, iterable_arg = []):
-		"""
-		function: add
-		argument: iterable: [int, float]
-		use: add([1,2,3,4,5])
-		description: returns the sum of elements in the iterable_arg, this is similar to sum
-		"""
-		try:
-			val_sum = 0
-			if iterable_arg:
-				for i in iterable_arg:
-					val_sum += i
-				
-				return val_sum
-			return val_sum
-		except Exception as e:
-			print(e)
+    def add(self, iterable_arg=[]):
+        """
+        function: add
+        argument: iterable: [int, float]
+        use: add([1,2,3,4,5])
+        description: returns the sum of elements in the iterable_arg, this is similar to sum
+        """
+        try:
+            val_sum = 0
+            if iterable_arg:
+                for i in iterable_arg:
+                    val_sum += i
 
-	def subtr(self, first_number = 0, second_number = 0):
-		"""
-		function: subtr
-		arguments: first_number:[int, float], second_number:[int, float]
-		use: subtr(1, 2.1)
-		description: returns the difference between first_number and second_number
-		"""
-		try:
-			return first_number - second_number
-		except Exception as e:
-			print(e)
+                return val_sum
+            return val_sum
+        except Exception as e:
+            print(e)
 
-	def multi(self, iterable_arg = []):
-		"""
-		function: multi
-		argument: iterable: [int, float]
-		use: multi([1,2,3,4,5])
-		description: returns the product of elements in the iterable.
-		"""
-		try:
-			val_sum = 1
-			if iterable_arg:
-				for i in iterable_arg:
-					val_sum *= i
-				
-				return val_sum
-			return 0
-		except Exception as e:
-			print(e)
+    def subtr(self, first_number=0, second_number=0):
+        """
+        function: subtr
+        arguments: first_number:[int, float], second_number:[int, float]
+        use: subtr(1, 2.1)
+        description: returns the difference between first_number and second_number
+        """
+        try:
+            return first_number - second_number
+        except Exception as e:
+            print(e)
 
-	def div(self, first_number = 0, second_number = 1):
-		"""
-		function: div
-		arguments: first_number:[int, float], second_number:[int, float]
-		use: div(1, 2.1)
-		description: returns a float when second_number divides first_number
-		"""
-		try:
-			if second_number:
-				return first_number / second_number
-			else:
-				print(f"second_number given {second_number}, use an int/float gt {0.0!r}")
-				return 0
-		except Exception as e:
-			print(e)
+    def multi(self, iterable_arg=[]):
+        """
+        function: multi
+        argument: iterable: [int, float]
+        use: multi([1,2,3,4,5])
+        description: returns the product of elements in the iterable.
+        """
+        try:
+            val_sum = 1
+            if iterable_arg:
+                for i in iterable_arg:
+                    val_sum *= i
 
-	def div_floor(self, first_number = 0, second_number = 1):
-		"""
-		function: div_floor
-		arguments: first_number:[int, float], second_number:[int, float]
-		use: div_floor(1, 2.1)
-		description: returns the quotient (int) when second_number divides first_number
-		pow
-		"""
-		try:
-			if second_number:
-				return first_number // second_number
-			else:
-				print(f"second_number given {second_number}, use an int/float gt {0.0!r}")
-				return 0
-		except Exception as e:
-			print(e)
+                return val_sum
+            return 0
+        except Exception as e:
+            print(e)
 
-	def mod(self, first_number = 0, second_number = 1):
-		"""
-		function: mod
-		arguments: first_number:[int, float], second_number:[int, float]
-		use: mod(1, 2.1)
-		description: returns a remainder when second_number divides first_number
-		"""
-		try:
-			if second_number:
-				return first_number % second_number
-			else:
-				print(f"second_number given {second_number}, use an int/float gt {0.0!r}")
-				return 0
-		except Exception as e:
-			print(e)
+    def div(self, first_number=0, second_number=1):
+        """
+        function: div
+        arguments: first_number:[int, float], second_number:[int, float]
+        use: div(1, 2.1)
+        description: returns a float when second_number divides first_number
+        """
+        try:
+            if second_number:
+                return first_number / second_number
+            else:
+                print(
+                    f"second_number given {second_number}, use an int/float gt {0.0!r}")
+                return 0
+        except Exception as e:
+            print(e)
 
-	def div_mod(self, first_number = 0, second_number = 1):
-		"""
-		function: div_mod
-		arguments: first_number:[int, float], second_number:[int, float]
-		use: div_mod(1, 2.1)
-		description: returns a tuple, of the quotient and  remainder when second_number divides first_number
-		"""
-		try:
-			if second_number:
-				return (first_number // second_number) , (first_number % second_number)
-			else:
-				print(f"second_number given {second_number}, use an int/float gt {0.0!r}")
-				return (0, 0)
-		except Exception as e:
-			print(e)
+    def div_floor(self, first_number=0, second_number=1):
+        """
+        function: div_floor
+        arguments: first_number:[int, float], second_number:[int, float]
+        use: div_floor(1, 2.1)
+        description: returns the quotient (int) when second_number divides first_number
+        pow
+        """
+        try:
+            if second_number:
+                return first_number // second_number
+            else:
+                print(
+                    f"second_number given {second_number}, use an int/float gt {0.0!r}")
+                return 0
+        except Exception as e:
+            print(e)
 
-	def pow(self, first_number = 1, second_number = 0):
-		"""
-		function: pow
-		arguments: first_number:[int, float], second_number:[int, float]
-		use: pow(1, 2.1)
-		description: returns first_number raised to the exponent, second_number
-		"""
-		try:
-			if first_number:
-				return first_number ** second_number
-			else:
-				print(f"first_number given {first_number}, use an int/float gt {0.0!r}")
-				return 0
-		except Exception as e:
-			print(e)
+    def mod(self, first_number=0, second_number=1):
+        """
+        function: mod
+        arguments: first_number:[int, float], second_number:[int, float]
+        use: mod(1, 2.1)
+        description: returns a remainder when second_number divides first_number
+        """
+        try:
+            if second_number:
+                return first_number % second_number
+            else:
+                print(
+                    f"second_number given {second_number}, use an int/float gt {0.0!r}")
+                return 0
+        except Exception as e:
+            print(e)
+
+    def div_mod(self, first_number=0, second_number=1):
+        """
+        function: div_mod
+        arguments: first_number:[int, float], second_number:[int, float]
+        use: div_mod(1, 2.1)
+        description: returns a tuple, of the quotient and  remainder when second_number divides first_number
+        """
+        try:
+            if second_number:
+                return (first_number // second_number), (first_number % second_number)
+            else:
+                print(
+                    f"second_number given {second_number}, use an int/float gt {0.0!r}")
+                return (0, 0)
+        except Exception as e:
+            print(e)
+
+    def pow(self, first_number=1, second_number=0):
+        """
+        function: pow
+        arguments: first_number:[int, float], second_number:[int, float]
+        use: pow(1, 2.1)
+        description: returns first_number raised to the exponent, second_number
+        """
+        try:
+            if first_number:
+                return first_number ** second_number
+            else:
+                print(
+                    f"first_number given {first_number}, use an int/float gt {0.0!r}")
+                return 0
+        except Exception as e:
+            print(e)
--- arithmatictest.py
+++ arithmatictest.py
@@ -6,125 +6,118 @@ import unittest
 
 
 class ArithmaticTest(unittest.TestCase):
-	app = Arithmatic()
+    app = Arithmatic()
 
-	def test_add_int_type(self):
-		self.assertTrue(type(self.app.add([1, 2]) == int))
+    def test_add_int_type(self):
+        self.assertTrue(type(self.app.add([1, 2]) == int))
 
-	def test_add_float_type(self):
-		self.assertTrue(type(self.app.add([1.2, 2.1]) == float))
+    def test_add_float_type(self):
+        self.assertTrue(type(self.app.add([1.2, 2.1]) == float))
 
-	def test_add_int_rval(self):
-		self.assertEqual(self.app.add([1, 2]), 3)
+    def test_add_int_rval(self):
+        self.assertEqual(self.app.add([1, 2]), 3)
 
-	def test_add_float_rval(self):
-		self.assertEqual(self.app.add([1.2, 2.1]), 3.3)
+    def test_add_float_rval(self):
+        self.assertEqual(self.app.add([1.2, 2.1]), 3.3)
 
-	def test_add_empty_ite(self):
-		self.assertEqual(self.app.add([]), 0)
+    def test_add_empty_ite(self):
+        self.assertEqual(self.app.add([]), 0)
 
-	def test_add_no_args(self):
-		self.assertEqual(self.app.add(), 0)
-		
+    def test_add_no_args(self):
+        self.assertEqual(self.app.add(), 0)
 
-	def test_subtr_int_type(self):
-		self.assertTrue(type(self.app.subtr(2, 1) == int))
+    def test_subtr_int_type(self):
+        self.assertTrue(type(self.app.subtr(2, 1) == int))
 
-	def test_subtr_float_type(self):
-		self.assertTrue(type(self.app.subtr(2.1, 1.2) == float))
-	
-	def test_subtr_int_rval(self):
-		self.assertEqual(self.app.subtr(2, 1), 1)
+    def test_subtr_float_type(self):
+        self.assertTrue(type(self.app.subtr(2.1, 1.2) == float))
 
-	def test_subtr_float_rval(self):
-		self.assertEqual(self.app.subtr(2.4, 1.2), 1.2) 
+    def test_subtr_int_rval(self):
+        self.assertEqual(self.app.subtr(2, 1), 1)
 
-	def test_subtr_empty_args(self): 
-		self.assertEqual(self.app.subtr(), 0)
-	
-	
-	def test_multi_int_type(self):
-		self.assertTrue(type(self.app.multi([2, 1])) == int)
+    def test_subtr_float_rval(self):
+        self.assertEqual(self.app.subtr(2.4, 1.2), 1.2)
 
-	def test_multi_float_type(self):
-		self.assertTrue(type(self.app.multi([2.1, 1.2])) == float)
+    def test_subtr_empty_args(self):
+        self.assertEqual(self.app.subtr(), 0)
 
-	def test_multi_int_rval(self):
-		self.assertEqual(self.app.multi([2, 1]), 2)
+    def test_multi_int_type(self):
+        self.assertTrue(type(self.app.multi([2, 1])) == int)
 
-	def test_multi_float_rval(self):
-		self.assertEqual(self.app.multi([2.4, 1.2]), 2.88) 
+    def test_multi_float_type(self):
+        self.assertTrue(type(self.app.multi([2.1, 1.2])) == float)
 
-	def test_multi_empty_args(self):
-		self.assertEqual(self.app.multi([]), 0)
+    def test_multi_int_rval(self):
+        self.assertEqual(self.app.multi([2, 1]), 2)
 
-	def test_multi_no_args(self):
-		self.assertEqual(self.app.multi(), 0)
+    def test_multi_float_rval(self):
+        self.assertEqual(self.app.multi([2.4, 1.2]), 2.88)
 
+    def test_multi_empty_args(self):
+        self.assertEqual(self.app.multi([]), 0)
 
-	def test_div_float_type(self):
-		self.assertTrue(type(self.app.div(5, 2)), float)
+    def test_multi_no_args(self):
+        self.assertEqual(self.app.multi(), 0)
 
-	def test_div_float_rval(self):
-		self.assertEqual(self.app.div(5, 2), 2.5)
+    def test_div_float_type(self):
+        self.assertTrue(type(self.app.div(5, 2)), float)
 
-	def test_div_epmty_args(self):
-		self.assertEqual(self.app.div(), 0)
+    def test_div_float_rval(self):
+        self.assertEqual(self.app.div(5, 2), 2.5)
 
-	def test_div_zero_division(self):
-		self.assertEqual(self.app.div(1, 0), 0)
+    def test_div_epmty_args(self):
+        self.assertEqual(self.app.div(), 0)
 
+    def test_div_zero_division(self):
+        self.assertEqual(self.app.div(1, 0), 0)
 
-	def test_divfloor_int_type(self):
-		self.assertTrue(type(self.app.div_floor(5, 2)), int)
+    def test_divfloor_int_type(self):
+        self.assertTrue(type(self.app.div_floor(5, 2)), int)
 
-	def test_divfloor_float_rval(self):
-		self.assertEqual(self.app.div_floor(5, 2), 2)
+    def test_divfloor_float_rval(self):
+        self.assertEqual(self.app.div_floor(5, 2), 2)
 
-	def test_divfloor_epmty_args(self):
-		self.assertEqual(self.app.div_floor(), 0)
+    def test_divfloor_epmty_args(self):
+        self.assertEqual(self.app.div_floor(), 0)
 
-	def test_divfloor_zero_division(self):
-		self.assertEqual(self.app.div(1, 0), 0)
+    def test_divfloor_zero_division(self):
+        self.assertEqual(self.app.div(1, 0), 0)
 
-	
-	def test_mod_int_type(self):
-		self.assertTrue(type(self.app.mod(23, 4)), int)
+    def test_mod_int_type(self):
+        self.assertTrue(type(self.app.mod(23, 4)), int)
 
-	def test_mod_int_rval(self):
-		self.assertEqual(self.app.mod(23, 4), 3)
+    def test_mod_int_rval(self):
+        self.assertEqual(self.app.mod(23, 4), 3)
 
-	def test_mod_empty_args(self):
-		self.assertEqual(self.app.mod(), 0)
+    def test_mod_empty_args(self):
+        self.assertEqual(self.app.mod(), 0)
 
-	def test_mod_zero_division(self):
-		self.assertEqual(self.app.mod(23, 0), 0)
+    def test_mod_zero_division(self):
+        self.assertEqual(self.app.mod(23, 0), 0)
 
+    def test_divmod_int_type(self):
+        self.assertTrue(type(self.app.div_mod(23, 4)), tuple)
 
-	def test_divmod_int_type(self):
-		self.assertTrue(type(self.app.div_mod(23, 4)), tuple)
+    def test_divmod_int_rval(self):
+        self.assertEqual(self.app.div_mod(23, 4), (5, 3))
 
-	def test_divmod_int_rval(self):
-		self.assertEqual(self.app.div_mod(23, 4), (5, 3))
+    def test_divmod_empty_args(self):
+        self.assertEqual(self.app.div_mod(), (0, 0))
 
-	def test_divmod_empty_args(self):
-		self.assertEqual(self.app.div_mod(), (0, 0))
+    def test_divmod_zero_division(self):
+        self.assertEqual(self.app.div_mod(23, 0), (0, 0))
 
-	def test_divmod_zero_division(self):
-		self.assertEqual(self.app.div_mod(23, 0), (0, 0))
+    def test_pow_int_type(self):
+        self.assertTrue(type(self.app.pow(2, 3)), int)
 
-	
-	def test_pow_int_type(self):
-		self.assertTrue(type(self.app.pow(2, 3)), int)
+    def test_pow_int_rval(self):
+        self.assertEqual(self.app.pow(2, 3), 8)
 
-	def test_pow_int_rval(self):
-		self.assertEqual(self.app.pow(2, 3), 8)
+    def test_pow_empty_args(self):
+        self.assertEqual(self.app.pow(), 1)
 
-	def test_pow_empty_args(self):
-		self.assertEqual(self.app.pow(), 1)
-
-	def test_pow_zero_base(self):
-		self.assertEqual(self.app.pow(0, 5), 0)
+    def test_pow_zero_base(self):
+        self.assertEqual(self.app.pow(0, 5), 0)
 
 
 unittest.main()
